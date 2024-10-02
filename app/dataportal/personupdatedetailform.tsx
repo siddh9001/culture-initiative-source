ee6f7d7e14da3cc6ce78ac248f5d46737e98c672
Siddhesh Patle <siddhup0109@gmail.com>
2024-10-02 19:53:33 +0530

    working on person data updation.

---
+++ app/dataportal/personupdatedetailform.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const personUpdateDetailForm = (props: Props) => {
+  return <div>personUpdateDetailForm</div>;
+};
+
+export default personUpdateDetailForm;
